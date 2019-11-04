dda91a429f20b687e413345b46e02da2fa2e91fa
sulih <w.sulih@yahoo.co.id>
2019-11-04 15:08:02 +0700

    dashboard

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,6 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b>Version</b> 2.4.0
     </div>
-    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Gilacoding</a>.</strong> All rights
-    reserved.
+    <strong>Copyright &copy; 2014-2016.</strong> All rights
+
   </footer>
