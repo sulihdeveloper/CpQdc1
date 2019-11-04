8760c7c21d814f44d337f29fe4c8cdad988e7e1a
sulih <w.sulih@yahoo.co.id>
2019-11-04 16:56:45 +0700

    layout

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -19,11 +19,11 @@
 
                <!-- Controls -->
                <a class="left carousel-control" href="#mycarousel" role="button" data-slide="prev">
-                 <!-- <span class="fa fa-chevron-left" aria-hidden="true"></span> -->
+                 <span class="fa fa-chevron-left" aria-hidden="true"></span>
                  <span class="sr-only">Previous</span>
                </a>
                <a class="right carousel-control" href="#mycarousel" role="button" data-slide="next">
-                 <!-- <span class="fa fa-chevron-right" aria-hidden="true"></span> -->
+                 <span class="fa fa-chevron-right" aria-hidden="true"></span>
                  <span class="sr-only">Next</span>
                </a>
              </div>
