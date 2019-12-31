ae8a7f9e9915a24574b7372d6e323029079f6dca
sulih <w.sulih@yahoo.co.id>
2019-12-31 16:40:52 +0700

    lof

--- resources/views/view_berita.blade.php
+++ resources/views/view_berita.blade.php
@@ -8,9 +8,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="js1/rs-plugin/css/settings.css" media="screen">
     <link rel="stylesheet" type="text/css" href="css1/isotope.css" media="screen">
+    <link rel="stylesheet" href="{{asset('css1/bootstrap.min.css')}}" />
     <link rel="stylesheet" href="css1/flexslider.css" type="text/css">
     <link rel="stylesheet" href="js1/fancybox/jquery.fancybox.css" type="text/css" media="screen">
-    <link rel="stylesheet" href="css1/bootstrap.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Serif:400,400italic,700|Open+Sans:300,400,600,700">
 
     <link rel="stylesheet" href="css1/style.css">
