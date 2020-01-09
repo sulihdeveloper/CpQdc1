1aeda267beef38f7a630273ff9232d75a13069c2
sulih <w.sulih@yahoo.co.id>
2020-01-09 11:28:59 +0700

    revisi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <!-- BASICS -->
     <meta charset="utf-8">
+
     <title>QDC Technologies</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'dashboard') }}</title>
+    <title> Admin QDC Technologies</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
