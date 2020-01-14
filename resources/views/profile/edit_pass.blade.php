af3dcf9279820282f2b406d4ab82aa16fad4b6f2
sulih <w.sulih@yahoo.co.id>
2020-01-14 17:12:28 +0700

    delete

--- resources/views/profile/edit_pass.blade.php
+++ resources/views/profile/edit_pass.blade.php
@@ -6,22 +6,22 @@
 		<h1>Edit {{$show->name}} Password</h1>
 		<div class="col-md-4">
 			<form method="POST" action="{{route('profile.update',$show->id)}}">
-				{{ csrf_field() }}		
-				{{ method_field('PATCH') }} 				
+				{{ csrf_field() }}
+				{{ method_field('PATCH') }}
 				<div class="form-group col-md-8">
 					<label for="password">New Password</label>
-					<input type="password" name="newpass" value="{{old('oldpass')}}" class="form-control">				
+					<input type="password" name="newpass" value="{{old('oldpass')}}" class="form-control">
 				</div>
 				<div class="form-group col-md-12">
 					<label for="password">Confirm Password</label>
-					<input type="password" name="passconf" value="{{old('oldpass')}}" class="form-control">				
+					<input type="password" name="passconf" value="{{old('oldpass')}}" class="form-control">
 				</div>
 				<div class="form-group col-md-6 col-md-offset-3">
 					<button class="btn btn-success">save</button>
 					<button class="btn btn-info">cancel</button>
-				</div>											
+				</div>
 				</form>
-			</form>
+
 		</div>
 		<div class="col-md-5">
 			@if (count($errors) > 0)
@@ -33,7 +33,7 @@
 			        </ul>
 			    </div>
 			@endif
-		</div>		
+		</div>
 	</div>
 </div>
 
