50586b229c1a8e4c4f7be43028de3d037139d5c4
sulih <w.sulih@yahoo.co.id>
2020-01-21 17:41:14 +0700

    text fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="utf-8">
     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon"/>
+    <link href="data:image/x-icon;base64,YourBase64StringHere" rel="icon" type="image/x-icon" />
     <title>QDC Technologies</title>
     <meta name="description" content="Website">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
